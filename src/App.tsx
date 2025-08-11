f481902bb644ce5524fdc2db3f4b9914abe2af8d
Davi Curty <davicurty850@gmail.com>
2025-08-11 14:42:02 -0300

    feat:starting project

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
-import "./App.css";
-import Header from "./components/header";
-import { ThemeProvider } from "./components/theme-provider";
-import Home from "./pages/Home";
-
 const App = () => {
-	return (
-		<div className="h-screen w-full bg-background">
-			<ThemeProvider>
-				<Header />
-				<Home />
-			</ThemeProvider>
-		</div>
-	);
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
 };
 
 export default App;
