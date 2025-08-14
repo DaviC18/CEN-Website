9466c65f671448ebd2a921140359ded654211e25
Davi Curty <davicurty850@gmail.com>
2025-08-14 16:18:03 -0300

    feat: active button and links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,27 +7,30 @@ const Navbar = () => {
     <div className="navbar max-sm:flex justify-between items-center fixed hidden bottom-6 left-1/2 transform-[translate(-50%)] w-9/10 px-8 py-1 z-30 bg-[rgba(181,0,253,0.06)] backdrop-blur-xl rounded-4xl  ">
       <a className="flex flex-col justify-center items-center cursor-pointer hover:text-violet-600">
         <span>
-          <House className="text-white text-xl" />
+          <House className="text-white text-xl active:text-violet-600" />
         </span>
-        <span className="text-white font-light text-xs tracking-normal">
+        <span className="text-white font-light text-xs tracking-normal active:text-violet-600">
           Home
         </span>
       </a>
       <a className="flex flex-col justify-center items-center cursor-pointer">
-        <Hourglass className="text-white text-xl " />
-        <span className="text-white font-light text-xs  tracking-normal">
+        <Hourglass className="text-white text-xl active:text-violet-600" />
+        <span className="text-white font-light text-xs  tracking-normal active:text-violet-600">
           História
         </span>
       </a>
       <a className="flex flex-col justify-center items-center max-xs cursor-pointer">
-        <PaperPlaneTilt className="text-white text-xl " />
-        <span className="text-white font-light text-xs  tracking-normal">
+        <PaperPlaneTilt className="text-white text-xl active:text-violet-600" />
+        <span className="text-white font-light text-xs  tracking-normal active:text-violet-600">
           Contato
         </span>
       </a>
       <a className="flex flex-col justify-center items-center cursor-pointer">
-        <CalendarDays className="text-white text-xl " width={20} />
-        <span className="text-white font-light text-xs  tracking-normal">
+        <CalendarDays
+          className="text-white text-xl active:text-violet-600"
+          width={20}
+        />
+        <span className="text-white font-light text-xs  tracking-normal active:text-violet-600">
           Calendário
         </span>
       </a>
--- src/components/magicui/interactive-hover-button.tsx
+++ src/components/magicui/interactive-hover-button.tsx
@@ -13,18 +13,18 @@ export const InteractiveHoverButton = React.forwardRef<
     <button
       ref={ref}
       className={cn(
-        "group relative max-sm:w-[110px] sm:w-[127px] cursor-pointer flex justify-center overflow-hidden rounded-full border-black bg-background p-2 px-6 max-sm:p-1 max-sm:px-2.5 min-[440px]:p-2 min-[440px]:px-2 min-[440px]:w-[127px] text-center font-semibold duration-600 hover:bg-black",
+        "group relative max-sm:w-[110px] sm:w-[127px] cursor-pointer flex justify-center overflow-hidden rounded-full border-black bg-background p-2 px-6 max-sm:p-1 max-sm:px-2.5 min-[440px]:p-2 min-[440px]:px-2 min-[440px]:w-[127px] text-center font-semibold duration-600 hover:bg-black focus:bg-black focus:outline-gray-100 focus:duration-100 focus:outline-2",
         className
       )}
       {...props}
     >
       <div className="flex items-center gap-2 ">
-        <div className="h-2 w-2 max-md:pl-2 max-sm:pl-0 rounded-full bg-primary transition-all duration-300 group-hover:scale-[100.8]"></div>
-        <span className="inline-block transition-all duration-300 group-hover:translate-x-12 group-hover:opacity-0">
+        <div className="h-2 w-2 max-md:pl-2 max-sm:pl-0 rounded-full bg-primary transition-all duration-300 group-hover:scale-[100.8] group-focus:scale-[100] "></div>
+        <span className="inline-block transition-all duration-300 group-hover:translate-x-12 group-hover:opacity-0 group-focus:translate-x-12 group-focus:opacity-0">
           {children}
         </span>
       </div>
-      <div className="absolute top-0 left-6/30 z-10 max-sm:px-2.5 flex h-full w-full translate-x-12 items-center max-sm:justify-center justify-center gap-1 text-primary-foreground opacity-0 transition-all duration-300 group-hover:-translate-x-5 group-hover:opacity-100">
+      <div className="absolute top-0 left-6/30 z-10 max-sm:px-2.5 flex h-full w-full translate-x-12 items-center max-sm:justify-center justify-center gap-1 text-primary-foreground opacity-0 transition-all duration-300 group-hover:-translate-x-5 group-hover:opacity-100 group-focus:-translate-x-5 group-focus:opacity-100">
         <span className="text-violet-600 max-sm:text-sm sm:text-[16px] min-[440px]:text-[15px]">
           {children}
         </span>
